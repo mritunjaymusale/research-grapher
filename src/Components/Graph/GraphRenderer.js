a641f92014b673cc9a6b80727acd046749f13d8e
Mritunjay Musale <mritunjaymusale@gmail.com>
2021-04-24 00:59:38 +0530

    added a quick and direty way of updating the graph

--- src/Components/Graph/GraphRenderer.js
+++ src/Components/Graph/GraphRenderer.js
@@ -1,58 +1,59 @@
 import SpriteText from "three-spritetext";
-import React, { useEffect, useRef } from "react";
+import React, { Component } from "react";
 import { D3GraphProcessor } from "./GraphDataProcessor";
 import { ForceGraph3D } from "react-force-graph";
-import { useSelector } from "react-redux";
-import watch from "redux-watch";
 import { store } from "../../store";
-
-export const GraphRenderer = (props) => {
-  const graphInstance = useSelector((state) => state.graphReducer.graph);
-  var graph = D3GraphProcessor.convertToD3Graph(graphInstance);
-  const { attributes, options, ...data } = graph;
-
-  const fgRef = useRef();
-
-  let paper_watcher = watch(store.getState, "arxivReducer.paper");
-  store.subscribe(
-    paper_watcher((newVal, oldVal, objectPath) => {
-      if (newVal !== oldVal) {
-       
-      //  TODO: find a way to force rerender the component
-      }
-    })
-  );
-
-  useEffect(() => {
-    const fg = fgRef.current;
-
-    fg.d3Force("link").distance((link) => 500);
-  }, []);
-
-  return (
-    <ForceGraph3D
-      width={window.innerWidth / 2.2}
-      height={window.innerHeight / 2}
-      ref={fgRef}
-      graphData={data}
-      nodeAutoColorBy="group"
-      nodeThreeObject={(node) => {
-        var truncated_id = truncate(node.id, 25);
-        const sprite = new SpriteText(truncated_id);
-        sprite.color = node.color;
-        sprite.textHeight = 18;
-        return sprite;
-      }}
-      onNodeClick={(node, event) => {
-        props.onClickNode(node);
-      }}
-      backgroundColor="#101020"
-      linkColor={() => "rgba(255,255,255,0.2)"}
-      nodeRelSize={1}
-    />
-  );
-};
 
 function truncate(str, n) {
   return str.length > n ? str.substr(0, n - 1) + "..." : str;
+}
+
+export class GraphRenderer extends Component {
+  state = {
+    graph: store.getState().graphReducer.graph,
+  };
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  componentDidMount() {
+    const fg = this.myRef.current;
+
+    fg.d3Force("link").distance((link) => 500);
+
+    store.getState().graphReducer.graph.on("nodeAdded", ({ key }) => {
+      this.forceUpdate();
+    });
+  }
+  render() {
+    var graph = this.state.graph;
+    graph = D3GraphProcessor.convertToD3Graph(graph);
+    const { attributes, options, ...data } = graph;
+
+    return (
+      <ForceGraph3D
+        width={window.innerWidth / 2.2}
+        height={window.innerHeight / 2}
+        ref={this.myRef}
+        graphData={data}
+        nodeAutoColorBy="group"
+        nodeThreeObject={(node) => {
+          var truncated_id = truncate(node.id, 25);
+          const sprite = new SpriteText(truncated_id);
+          sprite.color = node.color;
+          sprite.textHeight = 18;
+          return sprite;
+        }}
+        onNodeClick={(node, event) => {
+          store.dispatch({
+            type: "UPDATE_CURRENTLY_SELECTED_NODE",
+            node: node,
+          });
+        }}
+        backgroundColor="#101020"
+        linkColor={() => "rgba(255,255,255,0.2)"}
+        nodeRelSize={1}
+      />
+    );
+  }
 }
